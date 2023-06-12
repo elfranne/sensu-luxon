5f439b5d9792e1c0fe598568858cd3b96cc593b2
Elfranne <tom.braarup@gmail.com>
2023-06-12 12:34:50 +0200

    wip

--- otto_tests/test.js
+++ otto_tests/test.js
@@ -1,44 +1,44 @@
 // Test to run with Otto vm
 
-function test_case1() { // summer time
-  var event = {
-    "entity": {
-      "metadata": {
-        "labels": {
-          "local_timezone": "Europe/Copenhagen" // CEST +2
-        },
-      },
-    },
-    "timestamp": 990810000 // 25 May 2001 17:00 UTC
-  }
-  var result = require('/lib/import.js').to_localtimezone(event);
-  if (result == 990817200  )  {  
-      console.log("Test PASSED ✅");
-  } else {
-      console.log("Test FAILED 🛑");
-      panic;
-  }
-}
+// function test_case1() { // summer time
+//   var event = {
+//     "entity": {
+//       "metadata": {
+//         "labels": {
+//           "local_timezone": "Europe/Copenhagen" // CEST +2
+//         },
+//       },
+//     },
+//     "timestamp": 990810000 // 25 May 2001 17:00 UTC
+//   }
+//   var result = require('/lib/import.js').to_localtimezone(event);
+//   if (result == 990817200  )  {  
+//       console.log("Test PASSED ✅");
+//   } else {
+//       console.log("Test FAILED 🛑");
+//       panic;
+//   }
+// }
 
-function test_case2() { // winter time
-  var event = {
-    "entity": {
-      "metadata": {
-        "labels": {
-          "local_timezone": "Europe/Copenhagen" // CET +1
-        },
-      },
-    },
-    "timestamp": 1703437200 // 24 Dec 2023 17:00 UTC
-  }
-  var result = require('/lib/import.js').to_localtimezone(event);
-  if (result == 1703440800  )  { 
-      console.log("Test PASSED ✅ ");
-  } else {
-      console.log("Test FAILED 🛑 ");
-      panic;
-  }
-}
+// function test_case2() { // winter time
+//   var event = {
+//     "entity": {
+//       "metadata": {
+//         "labels": {
+//           "local_timezone": "Europe/Copenhagen" // CET +1
+//         },
+//       },
+//     },
+//     "timestamp": 1703437200 // 24 Dec 2023 17:00 UTC
+//   }
+//   var result = require('/lib/import.js').to_localtimezone(event);
+//   if (result == 1703440800  )  { 
+//       console.log("Test PASSED ✅ ");
+//   } else {
+//       console.log("Test FAILED 🛑 ");
+//       panic;
+//   }
+// }
 
 console.log("--- Loading tests ---");
 
